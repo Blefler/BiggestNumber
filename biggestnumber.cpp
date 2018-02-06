6e3eb274005f3ac2810eabdc8ce05b13a987cc63
Blefler <blefler@mail.csuchico.edu>
2018-02-05 17:34:56 -0800

    The program will now tell you which number is bigger.

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -7,12 +7,18 @@ int main()
 {
 int numberone;
 int numbertwo;
-
+int biggest;
   cout<<"Please enter a whole number:\n";
 cin>>numberone;  
   cout<<"Please enter another whole number:\n";
 cin>>numbertwo;
-  cout<<"Of those two numbers, the biggest is: ";
+if (numberone>numbertwo)
+{biggest=numberone;
+}
+else
+{biggest=numbertwo;
+}
+  cout<<"Of those two numbers, the biggest is:" << biggest;
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
