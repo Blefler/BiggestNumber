2d5f79f2495f304481d48889c13040792125e438
Blefler <blefler@mail.csuchico.edu>
2018-02-05 17:14:24 -0800

    Stores user inserted numbers as integers

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -5,11 +5,13 @@ using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
-  cout<<"Please enter a whole number:\n";
-  
-  cout<<"Please enter another whole number:\n";
+int numberone;
+int numbertwo;
 
+  cout<<"Please enter a whole number:\n";
+cin>>numberone;  
+  cout<<"Please enter another whole number:\n";
+cin>>numbertwo;
   cout<<"Of those two numbers, the biggest is: ";
   cout<<endl<<"Thank you for playing.\n";
 
